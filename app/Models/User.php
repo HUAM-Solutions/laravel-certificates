cd66ae17991dbadbfecb29f0c544d0881dcd739f
Noe <noe@gmail.com>
2023-05-18 15:17:04 -0500

    prueba

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+    /*mutator-eloquent */
+    public function setPasswordAttribute($value)
+    {
+        $this->attibutes['password'] = bcrypt($value);
+    }
 }
