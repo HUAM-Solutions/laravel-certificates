006cc032d6930cde8559785b8cf626d3e137bfb1
Noe <noe@gmail.com>
2023-05-22 09:14:50 -0500

    22/05/2023

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,10 +5,14 @@ use App\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
+
 class LoginController extends Controller
 {
     //
     public function show(){
+        if (Auth::check()) {
+            return redirect('/home');
+        }
         return view('auth.login');
     }
     public function login(LoginRequest $request){
