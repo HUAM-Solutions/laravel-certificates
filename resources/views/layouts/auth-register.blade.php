de78a61239bcb7228304f7ee86839c7b360c9682
Nicole <janenieggcs@gmail.com>
2023-06-24 13:11:01 -0500

    Se arreglo el diseño de login y register

--- resources/views/layouts/auth-register.blade.php
+++ resources/views/layouts/auth-register.blade.php
@@ -12,50 +12,58 @@
 
 </head>
 <style>
-    body {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100vh; /* Asegúrate de que el body ocupe el 100% del alto de la ventana */
+
+
+    body{
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
     }
 
-    .background-container {
-        position: absolute;
-        z-index: -1;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        min-height: 100%;
+    body{
+    background: linear-gradient(318deg, #0E2334, #00CCCB, #0E2334, #00CCCB, #0E2334);
+    background-size: 1000% 1000%;
+    
+    -webkit-animation: Coolgradient 41s linear infinite;
+    -moz-animation: Coolgradient 41s linear infinite;
+    -o-animation: Coolgradient 41s linear infinite;
+    animation: Coolgradient 41s linear infinite;
     }
 
-    .form-container {
-        opacity: 0.9;
-        width: 400px;
-        margin-top: 25%;
-        position: relative;
-        z-index: 1; /* Asegúrate de que el z-index sea mayor que el del fondo SVG */
+    @-webkit-keyframes Coolgradient {
+        0%{background-position:5% 0%}
+        50%{background-position:96% 100%}
+        100%{background-position:5% 0%}
+    }
+    @-moz-keyframes Coolgradient {
+        0%{background-position:5% 0%}
+        50%{background-position:96% 100%}
+        100%{background-position:5% 0%}
+    }
+    @-o-keyframes Coolgradient {
+        0%{background-position:5% 0%}
+        50%{background-position:96% 100%}
+        100%{background-position:5% 0%}
+    }
+    @keyframes Coolgradient { 
+        0%{background-position:5% 0%}
+        50%{background-position:96% 100%}
+        100%{background-position:5% 0%}
     }
 
+    .bg{
+        background-color: #0E2334;
+        height: 120vh;
+    }
 
-    svg {
-		position: absolute;
-		top: 0;
-		left: 0;
-        width: 100%;
-        height: 160vh; /**Falta arreglar */
-		box-sizing: border-box;
-		display: block;
-		background-color: #0e4166;
-        background-image: linear-gradient(to bottom, rgba(14, 65, 102, 0.86), #0e4166);
-	}
-
-    .btn{
+    .btn-register{
         background-color: #00CCCB;
         color: #0E2334;
+        
     }
 
-    .btn:hover{
+    .btn-register:hover{
         background-color: #001E31;
         color: #FFFFFF;
     }
@@ -70,60 +78,73 @@
         color: #00CCCB; 
     }
 
+    .carousel-control-slide{
+        margin-top: 10%;
+        
+    }
+    .carousel-control-slide img{
+        border-radius: 15px;
+    }
+    .container{
+        background-color: #0E2334;
+    }
+
     .fa-solid{
         color: #c3132e;
     }
-
-    .header-color-auth{
-        padding-top: 2%;
-        background-color: #0E2334;
-        color: #FFFFFF;
-        border-radius: 10px;
+    .form-container{
+        width: 100%;
     }
+
+    .logo{
+        margin-top: 10%;
+        height: 10%;
+    }
+
 
 </style>
 
 <body>
 
-    <div class="background-container">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            width="100%" height="100%" viewBox="0 0 1700 900" preserveAspectRatio="xMidYMax slice">
-            <defs>
-                <linearGradient id="bg">
-                    <stop offset="0%" style="stop-color:rgba(130, 158, 249, 0.06)"></stop>
-                    <stop offset="50%" style="stop-color:rgba(76, 190, 255, 0.6)"></stop>
-                    <stop offset="100%" style="stop-color:rgba(115, 209, 72, 0.2)"></stop>
-                </linearGradient>
-                <path id="wave" fill="url(#bg)" d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
-        s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z" />
-            </defs>
-            <g>
-                <use xlink:href='#wave' opacity=".3">
-                    <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="10s"
-                        calcMode="spline" values="270 230; -334 180; 270 230" keyTimes="0; .5; 1"
-                        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" />
-                </use>
-                <use xlink:href='#wave' opacity=".6">
-                    <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="8s"
-                        calcMode="spline" values="-270 230;243 220;-270 230" keyTimes="0; .6; 1"
-                        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" />
-                </use>
-                <use xlink:href='#wave' opacty=".9">
-                    <animateTransform attributeName="transform" attributeType="XML" type="translate" dur="6s"
-                        calcMode="spline" values="0 230;-140 200;0 230" keyTimes="0; .4; 1"
-                        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0" repeatCount="indefinite" />
-                </use>
-            </g>
-        </svg>
+<div class="container w-75 mt-5 mb-5 rounded shadow">
+        <div class="row align-items-stretch">
+            <div class="col bg-white py-5 rounded-end">
+                <div >
+                    <h2 class="fw-bold text-center ">Registro de usuario</h2>
+                    <hr/>
+                </div>
+                <main class="form-container">
+                    @yield('content')
+                </main>
+            </div>
+            <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded">
+                <img class="logo" src="assets/img/logo_web_verde.png">
+                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active carousel-control-slide">
+                        <img src="assets/img/brigadas.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item carousel-control-slide">
+                        <img src="assets/img/brigadas-1.png" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item carousel-control-slide">
+                        <img src="assets/img/post.png" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+
     </div>
     
-    <main class="form-container">
-
-        @yield('content')
-    </div>
-
-    
-
     <script src="{{ url('assets/js/bootstrap.bundle.min.js')}}"></script>
 </body>
 
